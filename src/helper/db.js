ce09cdde3137a24dc1930f3122cb19ff7d75888e
bara0411 <bara041197@gmail.com>
2021-06-20 21:43:08 +0530

    heroku

--- src/helper/db.js
+++ src/helper/db.js
@@ -1,7 +1,6 @@
-
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://poc_fitpay:9842164730@poc.9v84r.mongodb.net/fitpay?retryWrites=true&w=majority`, (err) => {
+mongoose.connect("mongodb+srv://poc_fitpay:9842164730@poc.9v84r.mongodb.net/fitpay?retryWrites=true&w=majority", (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
